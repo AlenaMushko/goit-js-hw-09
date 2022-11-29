8772760e240821053684d15ac135fe51de08a5e5
alena <evstafienko_a@ukr.net>
2022-11-29 11:48:56 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,29 +13,11 @@ const refs = {
 };
 const PROMPT_DELEY = 1000;
 let selectedTime = null;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: Date.now(),
-  minuteIncrement: 1,
-  //   Регулює крок для введення хвилин (включно з прокручуванням
-  onClose(selectedDates) {
-    if (selectedDates[0] <= Date.now()) {
-      Notify.warning('Please choose a date in the future');
-      refs.btnStart.disabled = true;
-    } else {
-      refs.btnStart.disabled = false;
-      selectedDates[0] = selectedTime;
-    }
-  },
-};
-
 class Timer {
   constructor() {
     this.intervalId = null;
     this.isActive = false;
-     }
+  }
 
   startTimer() {
     if (this.isActive) {
@@ -50,8 +32,6 @@ class Timer {
       const time = convertMs(deltaTime);
       this.faceTimer(time);
 
-      console.log(currentTime);
-      console.log(selectedTime);
       if (deltaTime <= 0) {
         //  Для остановки таймера используется функция clearInterval, которая
         //  принимает уникальный номер того таймера, который нужно остановить.
@@ -61,42 +41,57 @@ class Timer {
     }, PROMPT_DELEY);
   }
 
-   faceTimer({ days, hours, minutes, seconds }) {
-  refs.daysEl.textContent = days;
-  refs.hoursEl.textContent = hours;
-  refs.minutesEl.textContent = minutes;
-  refs.secondsEl.textContent = seconds;
-}
-}
-
- function   convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = addLeadingZero(
-      Math.floor((((ms % day) % hour) % minute) / second)
-    );
-
-    return { days, hours, minutes, seconds };
+  faceTimer({ days, hours, minutes, seconds }) {
+    refs.daysEl.textContent = days;
+    refs.hoursEl.textContent = hours;
+    refs.minutesEl.textContent = minutes;
+    refs.secondsEl.textContent = seconds;
   }
+}
+const options = flatpickr(refs.inputEl,  {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: Date.now(),
+  minuteIncrement: 1,
+  //   Регулює крок для введення хвилин (включно з прокручуванням
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      Notify.warning('Please choose a date in the future');
+      refs.btnStart.disabled = true;
+      selectedTime = defaultDate;
+    } else {
+      refs.btnStart.disabled = false;
+      selectedTime = selectedDates[0];
+    }
+  },
+});
 
-function  addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-  }
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
 
 const timer = new Timer();
-flatpickr(refs.inputEl, options);
+
 // Функція bind()створює нову зв'язану функцію
 refs.btnStart.addEventListener('click', timer.startTimer.bind(timer));
-
-
