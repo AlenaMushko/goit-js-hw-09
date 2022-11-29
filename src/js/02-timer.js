c8ece207138493a85cac9534625ecf7f1b70a9f0
alena <evstafienko_a@ukr.net>
2022-11-29 11:07:12 +0200

    2,5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -14,74 +14,10 @@ const refs = {
 const PROMPT_DELEY = 1000;
 let selectedTime = null;
 
-class Timer {
-  constructor({ onTick }) {
-    this.intervalId = null;
-    this.isActive = false;
-    this.onTick = onTick;
-    this.init();
-  }
-
-  init() {
-    const time = this.convertMs(0);
-    this.onTick(time);
-  }
-
-  startTimer() {
-    if (this.isActive) {
-      return;
-    }
-
-    this.isActive = true;
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      // залишок часу
-      const deltaTime = selectedTime - currentTime;
-      const time = this.convertMs(deltaTime);
-      this.onTick(time);
-      if (deltaTime <= 0) {
-        //  Для остановки таймера используется функция clearInterval, которая
-        //  принимает уникальный номер того таймера, который нужно остановить.
-        clearInterval(this.intervalId);
-        this.isActive = false;
-        const time = this.convertMs(0);
-        this.onTick(time);
-      }
-    }, PROMPT_DELEY);
-  }
-
-    convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = this.addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = this.addLeadingZero(
-      Math.floor((((ms % day) % hour) % minute) / second)
-    );
-
-    return { days, hours, minutes, seconds };
-  }
-
-  addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-  }
-}
-
-const options = flatpickr(refs.inputEl, {
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: Date.now(),
-  // maxDate: '2022-12-19 00:00',
-  dateFormat: 'Y-m-d    H:i',
   minuteIncrement: 1,
   //   Регулює крок для введення хвилин (включно з прокручуванням
   onClose(selectedDates) {
@@ -93,16 +29,74 @@ const options = flatpickr(refs.inputEl, {
       selectedDates[0] = selectedTime;
     }
   },
-});
+};
 
+class Timer {
+  constructor() {
+    this.intervalId = null;
+    this.isActive = false;
+     }
 
-const timer = new Timer({ onTick: faceTimer });
-// Функція bind()створює нову зв'язану функцію
-refs.btnStart.addEventListener('click', timer.startTimer.bind(timer));
+  startTimer() {
+    if (this.isActive) {
+      return;
+    }
 
-function faceTimer({ days, hours, minutes, seconds }) {
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      // залишок часу
+      const deltaTime = selectedTime - currentTime;
+      const time = convertMs(deltaTime);
+      this.faceTimer(time);
+
+      console.log(currentTime);
+      console.log(selectedTime);
+      if (deltaTime <= 0) {
+        //  Для остановки таймера используется функция clearInterval, которая
+        //  принимает уникальный номер того таймера, который нужно остановить.
+        clearInterval(this.intervalId);
+        this.isActive = false;
+      }
+    }, PROMPT_DELEY);
+  }
+
+   faceTimer({ days, hours, minutes, seconds }) {
   refs.daysEl.textContent = days;
   refs.hoursEl.textContent = hours;
   refs.minutesEl.textContent = minutes;
   refs.secondsEl.textContent = seconds;
 }
+}
+
+ function   convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
+  }
+
+function  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+
+const timer = new Timer();
+flatpickr(refs.inputEl, options);
+// Функція bind()створює нову зв'язану функцію
+refs.btnStart.addEventListener('click', timer.startTimer.bind(timer));
+
+
