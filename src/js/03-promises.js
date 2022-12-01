5620ae0ee9ea8a971f448f581049fdcbf37aa2f3
alena <evstafienko_a@ukr.net>
2022-12-01 15:55:53 +0200

    3.1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,79 +1,57 @@
-// 1) користувач буде вводити першу затримку в мілісекундах,
-// 2) Для відображення повідомлень користувачеві, замість console.log(), використовуй
-// бібліотеку notiflix.
-// <form class="form">
-//   <label>
-//     First delay (ms)
-//     <input type="number" name="delay" required />
-//   <label>
-//     Delay step (ms)
-//     <input type="number" name="step" required />
-//   <label>
-//     Amount
-//     <input type="number" name="amount" required />
-//   <button type="submit">Create promises</button
-
 import Notiflix from 'notiflix';
 
 const refs = {
   formEl: document.querySelector('.form'),
-}
+};
+let MAX_PROMPT = 0;
+let position = 0;
+let delayValue = 0;
 
 refs.formEl.addEventListener('submit', onFormElSubmit);
+// refs.formEl.addEventListener('input', onFormElInput);
 
-function onFormElSubmit() {
-  createPromise(position, delay);
-
+function onFormElSubmit(e) {
+  // заборона по замовчуванню
+  e. preventDefault();
+position += 1;
+  onFormElInput();
+  
 }
-// Напиши скрипт, який на момент сабміту форми викликає функцію
-// createPromise(position, delay) стільки разів, скільки ввели в поле amount.
-//  Під час кожного виклику передай їй номер промісу (position), що створюється,
-//   і затримку, враховуючи першу затримку (delay), введену користувачем,
-//   і крок (step).
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+function onFormElInput() {
+  const { delay, step, amount } = refs.formEl.elements;
+ delayValue = delay.value;
+  MAX_PROMPT = amount.value;
+  // виклик функцііі MAX_PROMPT разів
+  // for( let position = 1; position === MAX_PROMPT )
+  if (position === MAX_PROMPT) {
+    createPromise(position, delayValue)
+  .then(({ position, delayValue }) => {
+    setTimeout(() => {
+    Notiflix.Report.success('Title', '✅ Fulfilled promise ${position} in ${delayValue}ms');
+    console.log(`✅ Fulfilled promise ${position} in ${delayValue}ms`);  
+    }, delayValue);
+  })
+  .catch(({ position, delayValue }) => {
+    setTimeout(() => {
+      Notiflix.Report.failure('Title', '❌ Rejected promise ${position} in ${delayValue}ms');
+    console.log(`❌ Rejected promise ${position} in ${delayValue}ms`);
+    }, delayValue);
+  });
+   return;
+  delayValue += step;
+ 
   }
 }
 
-// Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
-//  який виконується або відхиляється через delay часу. Значенням промісу повинен
-//  бути об'єкт, в якому будуть властивості position і delay зі значеннями
-//  однойменних параметрів. Використовуй початковий код функції для вибору того,
-//   що потрібно зробити з промісом - виконати або відхилити.
-
-createPromise(position, delay)
-  .then(({ position, delay }) => {
-    Notiflix.Report.success('Title', '✅ Fulfilled promise ${position} in ${delay}ms');
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Report.failure('Title', '❌ Rejected promise ${position} in ${delay}ms');
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function createPromise(position, delayValue) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    resolve({position, delayValue});
+  } else {
+    Reject({position, delayValue});
+  }
+}
 
 
 
