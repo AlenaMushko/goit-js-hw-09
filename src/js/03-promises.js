ac87e590632c2eb747bcea7e502eae3470cf1b2b
alena <evstafienko_a@ukr.net>
2022-12-01 09:02:10 +0200

    3.0

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,61 +1,35 @@
-// користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки
-//  для кожного промісу після першого і кількість промісів, яку необхідно створити.
+// 1) користувач буде вводити першу затримку в мілісекундах,
+// 2) Для відображення повідомлень користувачеві, замість console.log(), використовуй
+// бібліотеку notiflix.
 // <form class="form">
 //   <label>
 //     First delay (ms)
 //     <input type="number" name="delay" required />
-//   </label>
 //   <label>
 //     Delay step (ms)
 //     <input type="number" name="step" required />
-//   </label>
 //   <label>
 //     Amount
 //     <input type="number" name="amount" required />
-//   </label>
-//   <button type="submit">Create promises</button>
-// </form>
+//   <button type="submit">Create promises</button
 
-// Напиши скрипт, який на момент сабміту форми викликає функцію 
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+}
+
+refs.formEl.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit() {
+  createPromise(position, delay);
+
+}
+// Напиши скрипт, який на момент сабміту форми викликає функцію
 // createPromise(position, delay) стільки разів, скільки ввели в поле amount.
 //  Під час кожного виклику передай їй номер промісу (position), що створюється,
-//   і затримку, враховуючи першу затримку (delay), введену користувачем, 
+//   і затримку, враховуючи першу затримку (delay), введену користувачем,
 //   і крок (step).
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-// Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
-//  який виконується або відхиляється через delay часу. Значенням промісу повинен 
-//  бути об'єкт, в якому будуть властивості position і delay зі значеннями 
-//  однойменних параметрів. Використовуй початковий код функції для вибору того,
-//   що потрібно зробити з промісом - виконати або відхилити.
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// Бібліотека повідомлень
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою 
-// додатковою практикою.
-
-// Для відображення повідомлень користувачеві, замість console.log(), використовуй
-// бібліотеку notiflix.
-
-
-
-
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
@@ -65,3 +39,41 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс,
+//  який виконується або відхиляється через delay часу. Значенням промісу повинен
+//  бути об'єкт, в якому будуть властивості position і delay зі значеннями
+//  однойменних параметрів. Використовуй початковий код функції для вибору того,
+//   що потрібно зробити з промісом - виконати або відхилити.
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Report.success('Title', '✅ Fulfilled promise ${position} in ${delay}ms');
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Report.failure('Title', '❌ Rejected promise ${position} in ${delay}ms');
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
